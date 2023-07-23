14ce883deb585c3f7a71676ca9b70635b910359d
murad-97 <muradalshorman136@gmail.com>
2023-07-23 19:26:58 +0300

    add

---
+++ quiz.js
@@ -0,0 +1,31 @@
+// let option = document.querySelectorAll(".option div");
+// let input = document.querySelectorAll("input");
+
+// option.forEach((element) => {
+// //   element.addEventListener("mouseover", () => {
+// //     element.style.backgroundColor = "#2699FB";
+// //     element.children[1].style.cssText = "background-color:#F8F9FD;color:black";
+// //     element.children[2].style.cssText = "color:white";
+// //   });
+// //   element.addEventListener("mouseout", () => {
+// //     element.style.backgroundColor = "white";
+// //     element.children[1].style.cssText = "background-color:#2699FB;color:white";
+// //     element.children[2].style.cssText = "color:black";
+// //   });
+// });
+// input.forEach((element) => {
+//   element.addEventListener("click", () => {
+//     input.forEach((element) => {
+//       element.parentElement.style.cssText = "background-color:white";
+//     });
+//     if (element.checked) {
+//       element.parentElement.style.cssText = "background-color:black";
+//       element.addEventListener("mouseout", () => {
+//         element.style.backgroundColor = "#2699FB";
+//         element.children[1].style.cssText =
+//           "background-color:#F8F9FD;color:black";
+//         element.children[2].style.cssText = "color:white";
+//       });
+//     }
+//   });
+// });
